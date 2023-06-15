46d75554ad7e76c715814eceb6e2cf01fc4db7a1
Alexander Zaytsev <65236626+ALexZzZay@users.noreply.github.com>
2023-06-15 12:24:24 +0300

    Untitled Pull Request (#2)
    * This is the entire test task
    
    * Add cornerRadius to viewImage
    Fix delay to 1 sec
    Add disabling YES/NO buttons after click
    
    * made button outlets private
    delete some spaces and empty strings

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -5,63 +5,44 @@ final class MovieQuizViewController: UIViewController {
     @IBOutlet private var textLabel: UILabel!
     @IBOutlet private var imageView: UIImageView!
     @IBOutlet private var counterLabel: UILabel!
+   
+    @IBOutlet private weak var yesButton: UIButton!
+    @IBOutlet private weak var noButton: UIButton!
     
     @IBAction private func noButtonCliked(_ sender: UIButton) {
-        
         let currentQuestion = questions[currentQuestionIndex]
         let givenAnswer = false
-        
-        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
-        
-        /*
-         Разберём, что здесь происходит
-         По индексу текущего вопроса находим в массиве нужный нам моковый вопрос;
-         Создаём константу, которая в зависимости от метода имеет значение «правда» или «ложь» соответственно;
-         Передаём в метод покраски рамок значение, сравнивая правильный ответ и ответ, который дал пользователь.
-         */
-        
-    }
-    @IBAction private func yesButtonCliked(_ sender: UIButton) {
-        
-        let currentQuestion = questions[currentQuestionIndex]
-        let givenAnswer = true
-        
+        self.noButton.isEnabled = false
+        self.yesButton.isEnabled = false
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
     
-    // вью модель для состояния "Вопрос показан"
+    @IBAction private func yesButtonCliked(_ sender: UIButton) {
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = true
+        self.yesButton.isEnabled = false
+        self.noButton.isEnabled = false
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+    }
+    
     struct QuizStepViewModel {
-        // картинка с афишей фильма с типом UIImage
         let image: UIImage
-        // вопрос о рейтинге квиза
         let question: String
-        // строка с порядковым номером этого вопроса (ex. "1/10")
         let questionNumber: String
     }
     
     struct QuizQuestion {
-        // строка с названием фильма
-        // совпадает с названием картинки афиши фильма в Assets
         let image: String
-        
-        // строка с вопросом о рейтинге фильма
         let text: String
-        
-        //булевое значение (true, false), правильный ответ на вопрос
         let correctAnswer: Bool
     }
     
-    // для состояния "Результат квиза"
     struct QuizResultsViewModel {
-      // строка с заголовком алерта
-      let title: String
-      // строка с текстом о количестве набранных очков
-      let text: String
-      // текст для кнопки алерта
-      let buttonText: String
+        let title: String
+        let text: String
+        let buttonText: String
     }
     
-    // массив вопросов
     private let questions: [QuizQuestion] = [
         QuizQuestion(
             image: "The Godfather",
@@ -105,19 +86,14 @@ final class MovieQuizViewController: UIViewController {
             correctAnswer: false)
     ]
     
-    // переменная с индексом текущего вопроса, начальное значение 0
-    // (по этому индексу будем искать вопрос в массиве, где индекс первого элемента 0, а не 1)
     private var currentQuestionIndex: Int = 0
-    
-    // переменная со счётчиком правильных ответов, начальное значение закономерно 0
     private var correctAnswers = 0
     
-    private func convert(model: QuizQuestion) -> QuizStepViewModel
-    {
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
         let questionStep = QuizStepViewModel(image: UIImage(named: model.image) ?? UIImage(),
                                              question: model.text,
-                                             questionNumber: "\(currentQuestionIndex + 1)/ \(questions.count)")
-        
+                                             questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
+    
         return questionStep
     }
     
@@ -125,11 +101,8 @@ final class MovieQuizViewController: UIViewController {
         counterLabel.text = step.questionNumber
         imageView.image = step.image
         textLabel.text = step.question
-        
     }
-    
-    // приватный метод для показа результатов раунда квиза
-    // принимает вью модель QuizResultsViewModel и ничего не возвращает
+
     private func show(quiz result: QuizResultsViewModel) {
         let alert = UIAlertController(
             title: result.title,
@@ -142,6 +115,7 @@ final class MovieQuizViewController: UIViewController {
             
             let firstQuestion = self.questions[self.currentQuestionIndex]
             let viewModel = self.convert(model: firstQuestion)
+            
             self.show(quiz: viewModel)
         }
         
@@ -150,31 +124,22 @@ final class MovieQuizViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
     
-    
-    // приватный метод, который меняет цвет рамки
-    // принимает на вход булевое значение и ничего не возвращает
     private func showAnswerResult(isCorrect: Bool) {
-       
+        
         if isCorrect {
             correctAnswers += 1
-            
         }
-        // метод красит рамку
+
         imageView.layer.masksToBounds = true // даем разрешение на рисование рамки
         imageView.layer.borderWidth = 8 // толщина рамки
-        
         imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
         
-        // запускаем задачу через 1 секунду с помощью деспетчера задач
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            // код который мы хотим вызвать через 1 секунду
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+
             self.showNextQuestionOrResults()
         }
-        
     }
-    
-    // приватный метод, который содержит логику перехода в один из сценариев
-    // метод ничего не принимает и ничего не возвращает
+
     private func showNextQuestionOrResults() {
         if currentQuestionIndex == questions.count - 1 {
             let text = "Ваш результат: \(correctAnswers)/10"
@@ -182,15 +147,19 @@ final class MovieQuizViewController: UIViewController {
                 title: "Этот раунд окончен!",
                 text: text,
                 buttonText: "Сыграть еще раз")
+            imageView.layer.borderColor = UIColor.clear.cgColor
+            self.noButton.isEnabled = true
+            self.yesButton.isEnabled = true
+            
             show(quiz: viewModel)
-            // идем в состояние результатов квиза
+            
         } else {
             currentQuestionIndex += 1
-            // идем в состояние вопрос показан
-            
             let nextQuestion = questions[currentQuestionIndex]
             let viewModel = convert(model: nextQuestion)
             imageView.layer.borderColor = UIColor.clear.cgColor
+            self.noButton.isEnabled = true
+            self.yesButton.isEnabled = true
             
             show(quiz: viewModel)
         }
@@ -208,67 +177,3 @@ final class MovieQuizViewController: UIViewController {
         show(quiz: convert(model: currentQuestion))
     }
 }
-
-/*
- Mock-данные
- 
- 
- Картинка: The Godfather
- Настоящий рейтинг: 9,2
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: The Dark Knight
- Настоящий рейтинг: 9
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: Kill Bill
- Настоящий рейтинг: 8,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: The Avengers
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: Deadpool
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: The Green Knight
- Настоящий рейтинг: 6,6
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: Old
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- 
- 
- Картинка: The Ice Age Adventures of Buck Wild
- Настоящий рейтинг: 4,3
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- 
- 
- Картинка: Tesla
- Настоящий рейтинг: 5,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- 
- 
- Картинка: Vivarium
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- */
