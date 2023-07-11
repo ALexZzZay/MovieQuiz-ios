0d8f261e10baf8ad41cc17d630fe04b0636057ed
Alexander Zaytsev <arkansaaaw@gmail.com>
2023-07-11 19:08:30 +0300

    This is entire 5-th sprint task

---
+++ MovieQuiz/Models/QuizStepViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  QuizStepViewModel.swift
+//  MovieQuiz
+//
+//  Created by ALeXzZzAy on 27.06.23.
+//
+
+import Foundation
+import UIKit
+
+struct QuizStepViewModel {
+    let image: UIImage
+    let question: String
+    let questionNumber: String
+}
